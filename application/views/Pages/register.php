b454202abecc91a231c563d21542e28339a44daa
amtaibu <isabiryetai645@gmail.com>
2022-01-17 19:12:03 +0300

    Added form validation on business admin registration form

--- application/views/Pages/register.php
+++ application/views/Pages/register.php
@@ -30,23 +30,24 @@
                     
                     <div class="tab-content border border-radius mt-4 p-sm-5 p-4" id="myTabContent">
                         <div class="tab-pane fade show active" id="login" role="tabpanel" aria-labelledby="login-tab">
-                            <form class="row t-sm-4 mt-2 mb-5 align-items-center" action="SubmitRegisterform" method="post">
+                            <form class="row t-sm-4 mt-2 mb-5 align-items-center" onsubmit ="return verifyPassword()"  action="SubmitRegisterform" method="post">
                                 <div class="mb-3 col-sm-12">
-                                    <input type="text" class="form-control" placeholder="Your Name" name="name">
+                                    <input type="text" class="form-control" placeholder="Your Name" name="name" required>
                                 </div>
                                 <div class="mb-3 col-sm-12">
-                                    <input type="email" class="form-control" placeholder="Email Address" name="email">
+                                    <input type="email" class="form-control" placeholder="Email Address" name="email" required>
                                 </div>
                                 <div class="mb-3 col-sm-12">
-                                    <input type="test" class="form-control" placeholder="Phone Number" name="phone">
+                                    <input type="test" class="form-control" placeholder="Phone Number" name="phone" required>
                                 </div>
                                 <div class="mb-3 col-sm-12">
-                                    <input type="Password" class="form-control" placeholder="Password" name="password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}">
+                                    <input type="Password" id="password"  class="form-control" placeholder="Password" name="password"  required>
                                 </div>
                                 <div class="mb-3 col-sm-12">
-                                    <input type="Password" class="form-control" placeholder="Confirm Password" name="password2">
+                                    <input type="Password" id="password2" class="form-control" placeholder="Confirm Password" name="password2" required>
                                 </div>
-                                <div id="message" class="mb-3 col-sm-12">
+                                <div  class="mb-3 col-sm-12">
+                                <p id="message"></p>
                             <p>Password must contain  a lowercase letter,capital (uppercase),a number and Minimum length of 8 characters
                                 
                             </p>
@@ -81,66 +82,35 @@ footer-->
     <!--=================================
 footer-->
 
-<script>
-var myInput = document.getElementById("password");
-var letter = document.getElementById("letter");
-var capital = document.getElementById("capital");
-var number = document.getElementById("number");
-var length = document.getElementById("length");
-
-// When the user clicks on the password field, show the message box
-myInput.onfocus = function() {
-  document.getElementById("message").style.display = "block";
-}
-
-// When the user clicks outside of the password field, hide the message box
-myInput.onblur = function() {
-  document.getElementById("message").style.display = "none";
-}
-
-// When the user starts to type something inside the password field
-myInput.onkeyup = function() {
-  // Validate lowercase letters
-  var lowerCaseLetters = /[a-z]/g;
-  if(myInput.value.match(lowerCaseLetters)) {
-    letter.classList.remove("invalid");
-    letter.classList.add("valid");
-  } else {
-    letter.classList.remove("valid");
-    letter.classList.add("invalid");
-}
-
-  // Validate capital letters
-  var upperCaseLetters = /[A-Z]/g;
-  if(myInput.value.match(upperCaseLetters)) {
-    capital.classList.remove("invalid");
-    capital.classList.add("valid");
-  } else {
-    capital.classList.remove("valid");
-    capital.classList.add("invalid");
-  }
-
-  // Validate numbers
-  var numbers = /[0-9]/g;
-  if(myInput.value.match(numbers)) {
-    number.classList.remove("invalid");
-    number.classList.add("valid");
-  } else {
-    number.classList.remove("valid");
-    number.classList.add("invalid");
-  }
-
-  // Validate length
-  if(myInput.value.length >= 8) {
-    length.classList.remove("invalid");
-    length.classList.add("valid");
-  } else {
-    length.classList.remove("valid");
-    length.classList.add("invalid");
-  }
-}
+<script>  
+function verifyPassword() {  
+  var pw = document.getElementById("password").value; 
+  var pw2 = document.getElementById("password2").value;   
+  //check empty password field  
+  if(pw == "") {  
+     document.getElementById("message").innerHTML = "**Fill the password please!";  
+     return false;  
+  }  
+   //minimum password length validation  
+  if(pw.localeCompare(pw2)!=0) {  
+     document.getElementById("message").innerHTML = "**Password Mismatch detected";  
+     return false;  
+  } 
+ //minimum password length validation  
+  if(pw.length < 8) {  
+     document.getElementById("message").innerHTML = "**Password length must be atleast 8 characters";  
+     return false;  
+  }  
+  
+//maximum length of password validation  
+  if(pw.length > 15) {  
+     document.getElementById("message").innerHTML = "**Password length must not exceed 15 characters";  
+     return false;  
+  } else {  
+     alert("Password is correct");  
+  }  
+}  
 </script>
-
 </body>
 
 </html>
