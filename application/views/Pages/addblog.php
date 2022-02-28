a0f56c5a35a3d1a9456ee01708941902715c0d57
amtaibu <isabiryetai645@gmail.com>
2022-02-28 23:02:37 +0300

    Added blog routing

--- application/views/Pages/addblog.php
+++ application/views/Pages/addblog.php
@@ -4,20 +4,7 @@
   $getdata= $this->session->userdata('loggedinUser');
    
 ?>
-<style>
-table.dataTable thead .sorting:after,
-table.dataTable thead .sorting:before,
-table.dataTable thead .sorting_asc:after,
-table.dataTable thead .sorting_asc:before,
-table.dataTable thead .sorting_asc_disabled:after,
-table.dataTable thead .sorting_asc_disabled:before,
-table.dataTable thead .sorting_desc:after,
-table.dataTable thead .sorting_desc:before,
-table.dataTable thead .sorting_desc_disabled:after,
-table.dataTable thead .sorting_desc_disabled:before {
-    bottom: .5em;
-}
-</style>
+
 <script>
 $(document).ready(function() {
     $('#dtBasicExample').DataTable();
